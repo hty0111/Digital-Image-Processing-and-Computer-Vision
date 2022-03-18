143bf6e8616e46309f1145d3800316c9997710fd
HTY <1044213317@qq.com>
2022-03-17 23:31:24 -0700

    add path

--- 3/code/src/main.cpp
+++ 3/code/src/main.cpp
@@ -9,18 +9,20 @@
 #include "../include/Noise.h"
 #include "../include/Filter.h"
 
-void showImage(const cv::Mat & mat, const std::string & win_name, cv::Size size, int wait_key) {
+void showImage(const cv::Mat & mat, const std::string & win_name, cv::Size size, int wait_key, const std::string & save_path="")
+{
     cv::namedWindow(win_name, cv::WINDOW_NORMAL);
     cv::resizeWindow(win_name, size);
     cv::imshow(win_name, mat);
     if (wait_key >= 0)
         cv::waitKey(wait_key);
+    if (!save_path.empty())
+        cv::imwrite(save_path, mat);
 }
 
 int main()
 {
     // read image
-//    cv::Mat input_image = imread("../../image/manipulator.png", cv::IMREAD_GRAYSCALE);
     cv::Mat input_image = cv::imread("../../image/gate.jpg");
     if (!input_image.data)
     {
@@ -28,30 +30,35 @@ int main()
         return -1;
     }
 
+    // set image size for display
     cv::Size image_size = cv::Size(1200, 900);
 
     Noise noise;
+    // Pepper noise
     showImage(input_image, "Input Image", image_size, 0);
     cv::Mat pepper_noise_image = noise.addPepperSaltNoise(input_image, input_image.cols * input_image.rows / 100);
-    showImage(pepper_noise_image, "pepper_salt Noise", image_size, 0);
-
-//    double sigma = 0;
+    showImage(pepper_noise_image, "pepper noise",
+              image_size, 0, "../../image/pepper_noise.png");
+    // Gaussian noise
     cv::Mat gauss_noise_image = noise.addGaussianNoise(input_image, 2, 0, 10);
-
-//    cv::createTrackbar("Kernel Size", "imgOut", &sigma, 20, noise.gaussCallback);
-    showImage(gauss_noise_image, "Gaussian Noise", image_size, 0);
+    showImage(gauss_noise_image, "Gaussian noise",
+              image_size, 0, "../../image/gauss_noise.png");
 
     Filter filter;
     // Mean Filter
     cv::Mat mean_pepper_image = filter.meanFilter(pepper_noise_image, cv::Size(5, 5));
-    showImage(mean_pepper_image, "Mean filter for pepper noise", image_size, 0);
+    showImage(mean_pepper_image, "Mean filter for pepper noise",
+              image_size, 0, "../../image/mean_pepper.png");
     cv::Mat mean_gauss_image = filter.meanFilter(gauss_noise_image, cv::Size(5, 5));
-    showImage(mean_gauss_image, "Mean filter for gaussian noise", image_size, 0);
+    showImage(mean_gauss_image, "Mean filter for gaussian noise",
+              image_size, 0, "../../image/mean_gauss.png");
     // Median Filter
     cv::Mat median_pepper_image = filter.medianFilter(pepper_noise_image, cv::Size(3, 3));
-    showImage(median_pepper_image, "Median filter for pepper noise", image_size, 0);
+    showImage(median_pepper_image, "Median filter for pepper noise",
+              image_size, 0, "../../image/median_pepper.png");
     cv::Mat median_gauss_image = filter.medianFilter(gauss_noise_image, cv::Size(3, 3));
-    showImage(median_gauss_image, "Median filter for gaussian noise", image_size, 0);
+    showImage(median_gauss_image, "Median filter for gaussian noise",
+              image_size, 0, "../../image/median_gauss.png");
     // Bilateral Filter
 //    cv::Mat bilateral_pepper_image = filter.bilateralFilter(pepper_noise_image);
 //    showImage(bilateral_pepper_image, "Bilateral filter for pepper image", image_size, 0);
